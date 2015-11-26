51cde13ac7319e99ccbeefc3f7a11dc932b60757
nicolas.matias <nicolas.matias0@gmail.com>
2015-11-26 17:23:38 +0100

    supression bar titre application sur la page acceuil

--- app/src/main/java/com/example/pierre_henrilibeert/edt/Acceuil.java
+++ app/src/main/java/com/example/pierre_henrilibeert/edt/Acceuil.java
@@ -1,16 +1,20 @@
 package com.example.pierre_henrilibeert.edt;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 
-public class Acceuil extends ActionBarActivity {
+public class Acceuil extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_acceuil);
     }
 
